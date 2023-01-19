4891717820e5a77203d4d870228a4ea5e2243d92
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-01-19 19:18:23 +0530

    fix: sidebar and navbar placement

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import store from "./store.js";
 function App() {
 	return (
 		<Provider store={store}>
-			<div className="grid grid-flow-col">
-				<Sidebar />
+			<div className="grid grid-flow-row">
+				<Navbar />
 				<div className="grid grid-cols-10">
-					<div className="col-start-1 col-span-10">
-						<Navbar />
+					<div className="grid grid-flow-col col-start-1 col-span-10">
+						<Sidebar />
 						<Body />
 					</div>
 				</div>
--- src/components/Body.js
+++ src/components/Body.js
@@ -3,10 +3,10 @@ import VideoList from "./VideoList";
 
 const Body = () => {
 	return (
-		<>
+		<div className="grid grid-flow-row">
 			<TagList />
 			<VideoList />
-		</>
+		</div>
 	);
 };
 

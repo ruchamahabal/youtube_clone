3286010230ee2f43b9f2ec3624c79f8add626a3f
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-01-19 20:04:11 +0530

    feat: routing and basic video page with embeds

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,26 +1,29 @@
 import moment from "moment";
+import { Link } from "react-router-dom";
 
 const VideoCard = ({ video }) => {
-	const { snippet, statistics } = video;
+	const { snippet, statistics, id } = video;
 	const { title, channelTitle, publishedAt, thumbnails } = snippet;
 
 	return (
-		<div className="flex flex-col flex-wrap gap-2 w-80 mb-2">
-			<img
-				alt="thumbnail"
-				className="rounded-lg w-80"
-				src={thumbnails?.standard?.url}
-			></img>
-			<h2 className="text-md font-bold flex-none">{title}</h2>
-			<h3 className="text-base">{channelTitle}</h3>
-			<div className="flex gap-2">
-				<p className="text-slate-500">{`${statistics?.viewCount} views`}</p>
-				<p className="text-slate-500">|</p>
-				<p className="text-slate-500">
-					{moment(publishedAt).fromNow()}
-				</p>
+		<Link to={"/watch?v=" + id}>
+			<div className="flex flex-col flex-wrap gap-2 w-80 mb-2 cursor-pointer">
+				<img
+					alt="thumbnail"
+					className="rounded-lg w-80"
+					src={thumbnails?.standard?.url}
+				></img>
+				<h2 className="text-md font-bold flex-none">{title}</h2>
+				<h3 className="text-base">{channelTitle}</h3>
+				<div className="flex gap-2">
+					<p className="text-slate-500">{`${statistics?.viewCount} views`}</p>
+					<p className="text-slate-500">|</p>
+					<p className="text-slate-500">
+						{moment(publishedAt).fromNow()}
+					</p>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 };
 
---
+++ src/components/watch/VideoPage.js
@@ -0,0 +1,11 @@
+import VideoPlayer from "./VideoPlayer";
+
+const VideoPage = () => {
+	return (
+		<div>
+			<VideoPlayer />
+		</div>
+	);
+};
+
+export default VideoPage;

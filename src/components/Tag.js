b56cfc9f73cf85370af5443c4189ca6394da23fa
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-01-28 12:39:36 +0530

    feat: Set tag as active

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,16 +1,19 @@
 import { useDispatch } from "react-redux";
 import { setFilteredVideoList } from "../slices/videoSlice";
 
-const Tag = ({ tagName }) => {
+const Tag = ({ tagName, setActive, active }) => {
 	const dispatch = useDispatch();
 
 	const filterVideos = () => {
+		setActive(tagName);
 		dispatch(setFilteredVideoList(tagName));
 	};
 
 	return (
 		<button
-			className="bg-gray-200 px-2 py-1 rounded-lg"
+			className={`px-2 py-1 rounded-lg ${
+				active == tagName ? "bg-black text-white" : "bg-gray-200"
+			}`}
 			onClick={filterVideos}
 		>
 			{tagName}
--- src/components/TagList.js
+++ src/components/TagList.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 import Tag from "./Tag.js";
 import { tags } from "../config.js";
 
 const TagList = () => {
+	const [active, setActive] = useState("");
+
 	return (
 		<div className="flex gap-3 mt-5 px-10 py-5">
 			{tags.map((tagName) => (
-				<Tag tagName={tagName} key={tagName} />
+				<Tag
+					tagName={tagName}
+					key={tagName}
+					active={active}
+					setActive={setActive}
+				/>
 			))}
 		</div>
 	);

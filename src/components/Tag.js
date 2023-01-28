e9d4dac11511e1a07688f2ebfc1660c299571e3a
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-01-28 12:18:05 +0530

    feat: tag-based filtering in videoList

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,6 +1,20 @@
+import { useDispatch } from "react-redux";
+import { setFilteredVideoList } from "../slices/videoSlice";
+
 const Tag = ({ tagName }) => {
+	const dispatch = useDispatch();
+
+	const filterVideos = () => {
+		dispatch(setFilteredVideoList(tagName));
+	};
+
 	return (
-		<button className="bg-gray-200 px-2 py-1 rounded-lg">{tagName}</button>
+		<button
+			className="bg-gray-200 px-2 py-1 rounded-lg"
+			onClick={filterVideos}
+		>
+			{tagName}
+		</button>
 	);
 };
 
